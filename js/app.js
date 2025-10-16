acf6a195155cc306a4dbce74f47d9e94a66124b7
Osvaldo_Enrique_Meza_Herrera <20243ds051@utez.edu.mx>
2025-10-15 18:21:24 -0600

    terminado

--- js/app.js
+++ js/app.js
@@ -1,28 +1,25 @@
-const nuevaTarea = document.getElementById("nuevaTarea");
-const btnAgregar = document.getElementById("btnAgregar");
-const lista = document.getElementById("lista-tareas");
+const nuevaTarea = document.querySelector(".nuevaTarea");
+const btnAgregar = document.querySelector(".btnAgregar");
+const lista = document.querySelector(".lista-tareas");
 
 const tareasGuardadas = localStorage.getItem("tareas");
-if (tareasGuardadas) {
-  lista.innerHTML = tareasGuardadas;
+if (tareasGuardadas != "" && tareasGuardadas != null) {
+    lista.innerHTML = tareasGuardadas;
 }
 
 btnAgregar.addEventListener("click", () => {
-  if (nuevaTarea.value.trim() === "") return;
-  const li = document.createElement("li");
-  li.textContent = nuevaTarea.value;
-  lista.appendChild(li);
-  nuevaTarea.value = "";
-  guardar();
+    const textoTarea = nuevaTarea.value;
+    if (textoTarea != "") {
+        lista.innerHTML += `<li> ${textoTarea}</li>`;
+        localStorage.setItem("tareas", lista.innerHTML);
+        nuevaTarea.value = "";
+    }
 });
 
 lista.addEventListener("click", (e) => {
-  if (e.target.tagName === "LI") {
-    e.target.classList.toggle("completada");
-    guardar();
-  }
+    if (e.target.tagName === "LI") {
+        e.target.style.textDecoration = "line-through";
+        e.target.style.color = "gray";
+        localStorage.setItem("tareas", lista.innerHTML);
+    }
 });
-
-function guardar() {
-  localStorage.setItem("tareas", lista.innerHTML);
-}
